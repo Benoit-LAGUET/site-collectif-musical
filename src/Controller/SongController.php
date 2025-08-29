7b8040853afc5ad4ad5f2dd6e826bc4c8b71e924
Benoit LAGUET <laguet.benoit@gmail.com>
2025-08-29 17:58:57 +0200

    feat: add new routes and methods for displaying and creating songs in SongController

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Song;
+use App\Form\SongType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SongController extends AbstractController
 {
@@ -17,6 +20,34 @@ class SongController extends AbstractController
 
         return $this->render('song/index.html.twig', [
             'songs' => $songs,
+        ]);
+    }
+
+    #[Route('/song/{id}', name: 'song_show', methods: ['GET'])]
+    public function show(Song $song): Response
+    {
+        return $this->render('song/show.html.twig', [
+            'song' => $song,
+        ]);
+    }
+
+    #[Route('/song/new', name: 'song_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $song = new Song();
+        $form = $this->createForm(SongType::class, $song);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($song);
+            $em->flush();
+
+            return $this->redirectToRoute('song_show', ['id' => $song->getId()]);
+        }
+
+        return $this->render('song/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
